c7fd895e7cae5a318d54a1542ef4edd6a419c575
pradeep-j-311 <156777745+pradeep-j-311@users.noreply.github.com>
2024-03-03 01:49:31 +0800

    Add colunmns to constructed dataframe from chat - Extract datetime characteristics from time structure - Add elapsed time since last message column - Clean up user identification from a given chat

--- process_chat.py
+++ process_chat.py
@@ -1,10 +1,17 @@
+
+# %%
 import re
 import pandas as pd
+import time 
+import matplotlib.pyplot as plt 
+from datetime import timedelta
+
 
 class whatsAppChat: 
 
     def __init__(self, path):
         self.chat_path = path
+        self.whatAppTimeFormat = '[%d/%m/%y, %H:%M:%S]'
 
     def chat_to_df(self): 
         with open(self.chat_path, encoding='utf-8') as f: 
@@ -12,56 +19,148 @@ class whatsAppChat:
             messages = chat.split('\n') # create a list out of the chat history 
 
         # remove date/time 
-        messages_woDateTime , DateTime = self.extractDateTime(messages)
-        # remove username
-        Content, Users = self.extractUsers(messages_woDateTime)
-        cols = [DateTime, Users, Content]
-        chat_df = pd.DataFrame(cols, columns=['DateTime', 'User', 'Message Content'])
+        DateTime, DateTimeObject, gmTime, month, day, year, hour, minute, second, days_since, hours_since, minutes_since, seconds_since, wDay, yDay, Users, Contents = self.extractInfo(messages)
+        data = {
+            "DateTime": DateTime, 
+            "DateTimeObject": DateTimeObject, 
+            "gmTime": gmTime, 
+            "Month": month, 
+            "Day": day,
+            "Year": year, 
+            "Hour": hour, 
+            "Minute": minute, 
+            "Seconds": second,
+            "DaysSince": days_since,
+            "HoursSince": hours_since, 
+            "MinutesSince": minutes_since, 
+            "SecondsSince": seconds_since,
+            "Day_Of_Week": wDay, 
+            "Day_Of_Year": yDay,
+            "User": Users, 
+            "Message": Contents
+        }
+        chat_df = pd.DataFrame(data)
 
         return chat_df
 
-    def extractDateTime(self, messages): 
+    def extractInfo(self, messages): 
 
         dt = re.compile(r'\[[0-9]*/[0-9]*/[0-9]*, [0-9]*:[0-9]*:[0-9]*\]') # regex expression for extracting the date and time 
         message_dt = [] # intialise list to store date and time 
-        cleaned_messages = []
+        month = []
+        day = []
+        day_of_week = []
+        hour = []
+        minute = []
+        year = []
+        seconds = []
+        day_of_year = []
+        message_dt_converted = []
+        time_delta = []
+        Users = []
+        Contents = []
+        elapsed_d = []
+        elapsed_h = []
+        elapsed_m = []
+        elapsed_s = []
+        gmTime = []
 
+        
+        # extract raw date/time values 
         for message in messages: 
             current_date = dt.search(message)
             if current_date != None: 
-                message_dt.append(current_date.group())
-                previous_date = current_date
+                extracted_date = current_date.group()
+                message_dt.append(extracted_date)
+
+                converted_date = time.strptime(extracted_date, self.whatAppTimeFormat)
+                gmTime.append(time.mktime(converted_date))
+                if len(time_delta) == 0: 
+                    time_delta.append(0)
+                    elapsed_d.append(0)
+                    elapsed_h.append(0)
+                    elapsed_m.append(0)
+                    elapsed_s.append(0)
+                else: 
+                    elapsed_time = time.mktime(converted_date) - time.mktime(message_dt_converted[-1])
+                    el_d, el_h, el_m, el_s = self.processElapsedTime(elapsed_time)
+                    elapsed_d.append(el_d)
+                    elapsed_h.append(el_h)
+                    elapsed_m.append(el_m)
+                    elapsed_s.append(el_s)
+                
+                message_dt_converted.append(converted_date)
+                month.append(converted_date.tm_mon)
+                day.append(converted_date.tm_mday)
+                day_of_week.append(converted_date.tm_wday)
+                hour.append(converted_date.tm_hour)
+                minute.append(converted_date.tm_min)
+                year.append(converted_date.tm_year)
+                seconds.append(converted_date.tm_sec)
+                day_of_year.append(converted_date.tm_yday)
                 # after the date/time has been extracted we remove it from the message line 
                 clean_message = dt.sub("", message)
-                cleaned_messages.append(clean_message)
+                user_content_split = re.split(':', clean_message, 1)
+                user = re.sub('\\u200e', '', user_content_split[0])
+                content = user_content_split[1]
+
+                Users.append(user)
+                Contents.append(content)
+
             else: 
                 # the way whatsapp exports message means that if a subsequent message is sent by the same user at a close enough time stamp then the data-time is not present
                 # in that line. For this we simply add the previous date/time to the message with this information missing
-                message_dt.append(previous_date.group())
-                cleaned_messages.append(message)
+                message_dt.append(message_dt[-1])
+                message_dt_converted.append(message_dt_converted[-1])
+                gmTime.append(gmTime[-1])
+                month.append(month[-1])
+                day.append(day[-1])
+                day_of_week.append(day_of_week[-1])
+                hour.append(hour[-1])
+                minute.append(minute[-1])
+                year.append(year[-1])
+                seconds.append(seconds[-1])
+                day_of_year.append(day_of_year[-1])
 
-        return cleaned_messages, message_dt 
+                Users.append(Users[-1])
+                Contents.append(message)
+                
+                elapsed_d.append(0)
+                elapsed_h.append(0)
+                elapsed_m.append(0)
+                elapsed_s.append(0)
 
-    def extractUsers(self, messages): 
-        cleaned_messages = [] 
-        message_users = []
-        for message in messages: 
-            # in whatsapp chat history export the users and the message content are seperated by ":"
-            # eg: Vidhi: Hello! 
-            # Name should not contain the ":" character 
-            current_split = re.split(":", message, 1)
-            if len(current_split) > 1: 
-                message_users.append(current_split[0])
-                cleaned_messages.append(current_split[1])
-                previous_user = current_split[0]
-            else: 
-                cleaned_messages.append(current_split[0])
-                message_users.append(previous_user)
-
-        return cleaned_messages, message_users 
+        return message_dt, message_dt_converted, gmTime, month, day, year, hour, minute, seconds, elapsed_d, elapsed_h, elapsed_m, elapsed_s, day_of_week, day_of_year, Users, Contents
     
-    def noOfMessages(self, dataframe): 
-        
+    def processElapsedTime(self, elapsed): 
+        daySeconds = 60*60*24
+        hourSeconds = 60*60
+        minuteSeconds = 60
+
+        elapsed_days = elapsed // daySeconds 
+        elapsed_seconds = elapsed % daySeconds
+        elapsed_hours = elapsed_seconds // hourSeconds
+        elapsed_seconds %= hourSeconds
+        elapsed_minutes = elapsed_seconds // minuteSeconds
+        elapsed_seconds %= minuteSeconds
+
+        return elapsed_days, elapsed_hours, elapsed_minutes, elapsed_seconds 
+    
+    def overallMetrics(self, dataframe): 
+
         TotalMessages = len(dataframe)
-        
-        return TotalMessages
+        chatusers = dataframe['User'].unique() 
+        NoOfMessages = []
+
+        for user in chatusers: 
+            nMessages = dataframe['User'].value_counts()[user]
+            NoOfMessages.append(nMessages)
+
+
+        return TotalMessages, NoOfMessages 
+# %%
+
+path = "sample_chats/sample02.txt"
+chat = whatsAppChat(path)
+df = chat.chat_to_df()
+# %%
